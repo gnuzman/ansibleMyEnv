ed52ab494a6f020a4d95714309e31bbd1a39bd10
house <94644378@qq.com>
2017-11-01 13:07:27 +0800

    kafka

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-ansible-playbook -i /root/ansibleMyEnv/environment/dev/zk /root/ansibleMyEnv/initZookeeper.yml
+./initZookeeper.sh
+./initKafka.sh
+
---
+++ initZookeeper.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ansible-playbook -i /root/ansibleMyEnv/environment/dev/zk /root/ansibleMyEnv/initZookeeper.yml
